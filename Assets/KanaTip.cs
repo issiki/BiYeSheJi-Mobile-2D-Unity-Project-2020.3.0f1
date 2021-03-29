ee9dcbeb4d19aee2fcee9d48cbd1c1022050f3a7
一色双葉IsshikiFutaba <futaba_isshiki@outlook.com>
2021-03-29 19:46:36 +0800

    Update KanaTip.cs

--- Assets/KanaTip.cs
+++ Assets/KanaTip.cs
@@ -8,18 +8,6 @@ public class KanaTip : MonoBehaviour
     public List<string> tips;
 
     System.Random rand = new System.Random();
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public string RandomTip()
     {
         var randIndex = rand.Next(0, tips.Count);
